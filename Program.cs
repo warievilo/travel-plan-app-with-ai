d4ba3c9297ee99122cae06b8ed4d40acd77522ea
Willians Vieira Oliveira <williansarievilo@gmail.com>
2023-05-30 23:15:36 -0300

    Added swagger reference for development purposes.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient(); 
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.Run();
